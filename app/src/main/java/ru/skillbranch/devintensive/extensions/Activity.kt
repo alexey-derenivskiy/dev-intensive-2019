68a11704ff0f5c06f581e29fbcd2fc1bc3e59498
alexey-derenivskiy <dap_1988@mail.ru>
2019-07-19 01:58:24 +1200

    Reliased **

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -3,6 +3,8 @@ package ru.skillbranch.devintensive.extensions
 import android.app.Activity
 import android.content.Context
 import android.graphics.Rect
+import android.view.View
+import kotlin.math.roundToLong
 import android.view.inputmethod.InputMethodManager
 
 fun Activity.hideKeyboard() {
@@ -10,16 +12,17 @@ fun Activity.hideKeyboard() {
     imm.hideSoftInputFromWindow(currentFocus?.windowToken, InputMethodManager.SHOW_FORCED)
 }
 
-//TODO - **
-/*
-fun Activity.isisKeyboardOpen(){
-    rootView.getWindowVisibleDisplayFrame(Rect())
+fun Activity.isKeyboardOpen(): Boolean
+{
+        val rootView = findViewById<View>(android.R.id.content)
+        val visibleBounds = Rect()
+        rootView.getWindowVisibleDisplayFrame(visibleBounds)
+        val heightDiff = rootView.height - visibleBounds.height()
+        val marginOfError = this.convertDpToPx(50F).roundToLong()
+        return heightDiff > marginOfError
 }
-*/
 
-//TODO - **
-/*
-fun Activity.isisKeyboardClosed(){
-    rootView.getWindowVisibleDisplayFrame(Rect())
+fun Activity.isKeyboardClosed(): Boolean {
+    return !this.isKeyboardOpen()
 }
-*/
+
